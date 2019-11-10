2b35c151192d34c7052670b36dfa0eef3d4db660
David Rehmat <me@dave.blue>
2019-11-10 03:24:27 -0500

    update to build

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-require('seneca')
-    ({tag: process.env.TAG || 'root'})
-    .use('consul-registry', {
-        host: process.env.CONSUL_HOST || '127.0.0.1'
-    })
-    .use('mesh', {
-        port: process.env.PORT || 31313,
-        isbase: true,
-        discover: {
-            registry: {
-                active: true
-            },
-            multicast: {
-                active: false
-            }
+const seneca = require('seneca');
+
+const service = seneca({
+    tag: process.env.TAG || 'root'
+});
+
+service.use('consul-registry', {
+    host: process.env.CONSUL_HOST || '127.0.0.1'
+});
+
+service.use('mesh', {
+    port: process.env.PORT || 31313,
+    isbase: true,
+    discover: {
+        registry: {
+            active: true
+        },
+        multicast: {
+            active: false
         }
-    })
-    .ready(function () {
-        console.log('ready, id = ', this.id)
-    });
+    }
+});
+
+service.ready(function () {
+    console.log('ready, id = ', this.id)
+});
